245f9c064eb9862c017fc5a31b6162b2878d96ed
br3w0r <br3w0r-concepts@outlook.com>
2021-07-05 20:54:48 +0300

    Type validation for Social.TypeID

--- repository/gamelist-repository.go
+++ repository/gamelist-repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"os"
 
 	"bitbucket.org/br3w0r/gamelist-backend/entity"
@@ -98,6 +99,16 @@ func (r *gameListRepository) GetAllPlatforms() []entity.Platform {
 
 func (r *gameListRepository) SaveProfile(profile entity.ProfileInfo) error {
 	// TODO: add socials binding
+	for i := range profile.Socials {
+		err := r.db.First(&entity.SocialType{}, profile.Socials[i].TypeID).Error
+		if err == gorm.ErrRecordNotFound {
+			return fmt.Errorf("couldn't find social of type %d", profile.Socials[i].TypeID)
+		}
+		if err != nil {
+			return err
+		}
+	}
+
 	return r.db.Save(&profile).Error
 }
 

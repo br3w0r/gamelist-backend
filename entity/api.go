b35546b44dbcc79c59ed86bbd80e0181dd2361e5
br3w0r <br3w0r-concepts@outlook.com>
2021-07-06 15:10:51 +0300

    Move structs that are not in db from gamelist.go to api.go

--- entity/api.go
+++ entity/api.go
@@ -4,3 +4,17 @@ type ProfileCreds struct {
 	Nickname string `json:"nickname"`
 	Email    string `json:"email"`
 }
+
+type ProfileInfo struct {
+	Model
+	Nickname    string   `gorm:"varchar(20);unique" json:"nickname" binding:"gte=2,lte=20"`
+	Description string   `gorm:"varchar(120)" json:"description" binding:"lte=120"`
+	GamesListed uint     `json:"games_listed"`
+	Socials     []Social `gorm:"foreignKey:ProfileID"`
+}
+
+type LoginProfile struct {
+	Nickname string `json:"nickname"`
+	Email    string `json:"email"`
+	Password string `gorm:"varchar(70);not null" json:"password" binding:"gte=6,lte=70"`
+}
--- entity/gamelist.go
+++ entity/gamelist.go
@@ -31,24 +31,10 @@ type Platform struct {
 	Name      string         `gorm:"varchar(20);unique;" json:"name"`
 }
 
-type ProfileInfo struct {
-	Model
-	Nickname    string   `gorm:"varchar(20);unique" json:"nickname" binding:"gte=2,lte=20"`
-	Description string   `gorm:"varchar(120)" json:"description" binding:"lte=120"`
-	GamesListed uint     `json:"games_listed"`
-	Socials     []Social `gorm:"foreignKey:ProfileID"`
-}
-
 type Profile struct {
 	ProfileInfo
 	Email    string `gorm:"unique;not null" json:"email" binding:"required"`
 	Password string `json:"password" binding:"gte=6,lte=70"`
-}
-
-type LoginProfile struct {
-	Nickname string `json:"nickname"`
-	Email    string `json:"email"`
-	Password string `gorm:"varchar(70);not null" json:"password" binding:"gte=6,lte=70"`
 }
 
 type Social struct {

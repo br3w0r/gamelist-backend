06fda00f9793a0ccde6c6f4ee80c485a6e7489ef
br3w0r <br3w0r-concepts@outlook.com>
2021-07-22 19:26:18 +0300

    Moved server creation to dedicated package

--- server.go
+++ server.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"strings"
 
-	"github.com/br3w0r/gamelist-backend/controller"
 	"github.com/br3w0r/gamelist-backend/helpers"
-	"github.com/br3w0r/gamelist-backend/repository"
-	"github.com/br3w0r/gamelist-backend/service"
-	test "github.com/br3w0r/gamelist-backend/test/stress"
-	"github.com/gin-gonic/gin"
+	"github.com/br3w0r/gamelist-backend/server"
 )
 
 var (
@@ -26,117 +20,19 @@ var (
 )
 
 func main() {
-	if FORCE_MIGRATE == "1" {
-		log.Println("Force migration.")
+	options := server.ServerOptions{
+		Production:         PRODUCTION_MODE == "1",
+		ServeStatic:        SERVE_STATIC == "1",
+		ForceMigrate:       FORCE_MIGRATE == "1",
+		ForceScrape:        FORCE_SCRAPE == "1",
+		StaticDir:          STATIC_DIR,
+		DatabaseDir:        DATABASE_DIR,
+		ScraperGRPCAddress: SCRAPER_GRPC_ADDRESS,
+		StressTest:         STRESS_TEST == "1",
+		StressTestOptions:  strings.Split(STRESS_TEST_OPTIONS, ","),
 	}
 
-	var dbDist string
-	if STRESS_TEST == "1" {
-		dbDist = DATABASE_DIR + "/stress.db"
-	} else {
-		dbDist = DATABASE_DIR + "/gamelist.db"
-	}
-
-	var (
-		// Repos
-		gamelistRepository repository.GamelistRepository = repository.NewGamelistRepository(dbDist, FORCE_MIGRATE == "1")
-
-		// Services
-		gamelistService service.GameListService = service.NewGameListService(gamelistRepository, SCRAPER_GRPC_ADDRESS)
-		jwtService      service.JWTService      = service.NewJWTService(gamelistRepository)
-
-		// Controllers
-		gamelistController controller.GameListController = controller.NewGameListController(gamelistService, jwtService)
-	)
-
-	if FORCE_SCRAPE == "1" {
-		log.Println("Force scraping.")
-
-		if STRESS_TEST == "1" {
-			gamelistService.ScrapeGames()
-		} else {
-			go gamelistService.ScrapeGames()
-		}
-	}
-
-	if STRESS_TEST == "1" {
-		test.RunStress(gamelistRepository, strings.Split(STRESS_TEST_OPTIONS, ","))
-		return
-	}
-
-	if PRODUCTION_MODE == "1" {
-		gin.SetMode(gin.ReleaseMode)
-	}
-
-	server := gin.Default()
-
-	if SERVE_STATIC == "1" {
-		server.Static("/css", STATIC_DIR+"/css")
-		server.Static("/js", STATIC_DIR+"/js")
-		server.LoadHTMLGlob(STATIC_DIR + "/*.html")
-
-		// For SPA on vue
-		server.NoRoute(func(ctx *gin.Context) {
-			ctx.HTML(http.StatusOK, "index.html", nil)
-		})
-	}
-
-	apiRoutes := server.Group("/api/v0")
-	{
-		apiRoutes.POST("/games/all",
-			gamelistController.Authorized,
-			gamelistController.GetAllGamesTyped,
-		)
-
-		apiRoutes.POST("/list-game",
-			gamelistController.Authorized,
-			gamelistController.ListGame,
-		)
-
-		apiRoutes.GET("/my-games",
-			gamelistController.Authorized,
-			gamelistController.GetMyGameList,
-		)
-
-		apiRoutes.POST("/games/search",
-			gamelistController.Authorized,
-			gamelistController.SearchGames,
-		)
-
-		apiRoutes.POST("/games/details",
-			gamelistController.Authorized,
-			gamelistController.GameDetails,
-		)
-
-		apiRoutes.POST("/profiles", gamelistController.PostProfile)
-
-		apiRoutes.POST("/aquire-tokens", gamelistController.AcquireJWTPair)
-		apiRoutes.POST("/refresh-tokens", gamelistController.RefreshJWTPair)
-		apiRoutes.POST("/revoke-token", gamelistController.RevokeRefreshToken)
-		apiRoutes.GET("/delete-all-refresh-tokens",
-			gamelistController.Authorized,
-			gamelistController.DeleteAllRefreshTokens,
-		)
-
-		// This will be replaced with gRPC admin shell
-		if PRODUCTION_MODE == "0" {
-			apiRoutes.POST("/games", gamelistController.PostGame)
-
-			apiRoutes.POST("/list-types", gamelistController.PostListType)
-			apiRoutes.GET("/list-types", gamelistController.GetAllListTypes)
-
-			apiRoutes.GET("/genres", gamelistController.GetAllGenres)
-			apiRoutes.POST("/genres", gamelistController.PostGenre)
-
-			apiRoutes.GET("/platforms", gamelistController.GetAllPlatforms)
-			apiRoutes.POST("/platforms", gamelistController.PostPlatform)
-
-			apiRoutes.GET("/profiles", gamelistController.GetAllProfiles)
-
-			apiRoutes.GET("/social-types", gamelistController.GetAllSocialtypes)
-			apiRoutes.POST("/social-types", gamelistController.PostSocialType)
-		}
-	}
+	server := server.NewServer(options)
 
 	server.Run(":8080")
 }
---
+++ server/main.go
@@ -0,0 +1,141 @@
+package server
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/br3w0r/gamelist-backend/controller"
+	"github.com/br3w0r/gamelist-backend/repository"
+	"github.com/br3w0r/gamelist-backend/service"
+	test "github.com/br3w0r/gamelist-backend/test/stress"
+	"github.com/gin-gonic/gin"
+)
+
+type ServerOptions struct {
+	Production         bool
+	ServeStatic        bool
+	ForceMigrate       bool
+	ForceScrape        bool
+	StaticDir          string
+	DatabaseDir        string
+	ScraperGRPCAddress string
+	StressTest         bool
+	StressTestOptions  []string
+}
+
+func NewServer(options ServerOptions) *gin.Engine {
+	if options.ForceMigrate {
+		log.Println("Force migration.")
+	}
+
+	var dbDist string
+	if options.StressTest {
+		dbDist = options.DatabaseDir + "/stress.db"
+	} else {
+		dbDist = options.DatabaseDir + "/gamelist.db"
+	}
+
+	var (
+		// Repos
+		gamelistRepository repository.GamelistRepository = repository.NewGamelistRepository(dbDist, options.ForceMigrate)
+
+		// Services
+		gamelistService service.GameListService = service.NewGameListService(gamelistRepository, options.ScraperGRPCAddress)
+		jwtService      service.JWTService      = service.NewJWTService(gamelistRepository)
+
+		// Controllers
+		gamelistController controller.GameListController = controller.NewGameListController(gamelistService, jwtService)
+	)
+
+	if options.ForceScrape {
+		log.Println("Force scraping.")
+
+		if options.StressTest {
+			gamelistService.ScrapeGames()
+		} else {
+			go gamelistService.ScrapeGames()
+		}
+	}
+
+	// First version. Should be remade
+	if options.StressTest {
+		test.RunStress(gamelistRepository, options.StressTestOptions)
+		return nil
+	}
+
+	if options.Production {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	server := gin.Default()
+
+	if options.ServeStatic {
+		server.Static("/css", options.StaticDir+"/css")
+		server.Static("/js", options.StaticDir+"/js")
+		server.LoadHTMLGlob(options.StaticDir + "/*.html")
+
+		// For SPA on vue
+		server.NoRoute(func(ctx *gin.Context) {
+			ctx.HTML(http.StatusOK, "index.html", nil)
+		})
+	}
+
+	apiRoutes := server.Group("/api/v0")
+	{
+		apiRoutes.POST("/games/all",
+			gamelistController.Authorized,
+			gamelistController.GetAllGamesTyped,
+		)
+
+		apiRoutes.POST("/list-game",
+			gamelistController.Authorized,
+			gamelistController.ListGame,
+		)
+
+		apiRoutes.GET("/my-games",
+			gamelistController.Authorized,
+			gamelistController.GetMyGameList,
+		)
+
+		apiRoutes.POST("/games/search",
+			gamelistController.Authorized,
+			gamelistController.SearchGames,
+		)
+
+		apiRoutes.POST("/games/details",
+			gamelistController.Authorized,
+			gamelistController.GameDetails,
+		)
+
+		apiRoutes.POST("/profiles", gamelistController.PostProfile)
+
+		apiRoutes.POST("/aquire-tokens", gamelistController.AcquireJWTPair)
+		apiRoutes.POST("/refresh-tokens", gamelistController.RefreshJWTPair)
+		apiRoutes.POST("/revoke-token", gamelistController.RevokeRefreshToken)
+		apiRoutes.GET("/delete-all-refresh-tokens",
+			gamelistController.Authorized,
+			gamelistController.DeleteAllRefreshTokens,
+		)
+
+		// This will be replaced with gRPC admin shell
+		if options.Production {
+			apiRoutes.POST("/games", gamelistController.PostGame)
+
+			apiRoutes.POST("/list-types", gamelistController.PostListType)
+			apiRoutes.GET("/list-types", gamelistController.GetAllListTypes)
+
+			apiRoutes.GET("/genres", gamelistController.GetAllGenres)
+			apiRoutes.POST("/genres", gamelistController.PostGenre)
+
+			apiRoutes.GET("/platforms", gamelistController.GetAllPlatforms)
+			apiRoutes.POST("/platforms", gamelistController.PostPlatform)
+
+			apiRoutes.GET("/profiles", gamelistController.GetAllProfiles)
+
+			apiRoutes.GET("/social-types", gamelistController.GetAllSocialtypes)
+			apiRoutes.POST("/social-types", gamelistController.PostSocialType)
+		}
+	}
+
+	return server
+}
